80c91d94bdd5cbf0d3f7c9c5b931285fc4bfa2b2
ragibson <ryan.alexander.gibson@gmail.com>
2020-08-09 13:35:14 -0400

    Improve simple_quadrature accuracy and add WIP warning to make this public

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # levy-stable-benchmarks
 
+### ***This is a work in progress. Please come back later, except for limited purposes and/or review.***
+
 Stable distributions (sometimes called Lévy alpha-stable distributions) are
 important for modelling data across several disciplines including signal
 processing, physics, and finance. Despite this, many Python libraries
 provide buggy and/or inaccurate implementations for computing its PDF/CDF.
 
-This repository attempts to define a benchmark to test the accuracy of such
-implementations.
+This repository attempts to define a benchmark to test the accuracy of
+such implementations. We also provide some alternative calculation methods.
 
 ![accuracy figures](figures/accuracy_figures.png)
 
@@ -25,7 +27,7 @@ implementations.
   <tr><td>Method</td><td>1E-2</td><td>1E-3</td><td>1E-4</td><td>1E-5</td><td></td></tr>
   <tr>
     <td>simple_quadrature</td>
-    <td>99.9%</td><td>99.9%</td><td>99.6%</td><td>99.4%</td>
+    <td>100.0%</td><td>100.0%</td><td>99.6%</td><td>99.4%</td>
     <td></td>
   </tr>
   <tr>
@@ -87,7 +89,7 @@ implementations.
   <tr><td>Method</td><td>1E-2</td><td>1E-3</td><td>1E-4</td><td>1E-5</td><td></td></tr>
   <tr>
     <td>simple_quadrature</td>
-    <td>98.6%</td><td>98.6%</td><td>98.5%</td><td>98.5%</td>
+    <td>99.2%</td><td>99.2%</td><td>99.2%</td><td>99.2%</td>
     <td></td>
   </tr>
   <tr>
@@ -117,7 +119,7 @@ implementations.
   <tr><td>Method</td><td>1E-1</td><td>1E-2</td><td>1E-3</td><td>1E-4</td><td></td></tr>
   <tr>
     <td>simple_quadrature</td>
-    <td>98.6%</td><td>98.6%</td><td>98.6%</td><td>98.5%</td>
+    <td>99.3%</td><td>99.2%</td><td>99.2%</td><td>99.2%</td>
     <td></td>
   </tr>
   <tr>
@@ -149,7 +151,7 @@ implementations.
   <tr><td>Method</td><td>1E-2</td><td>1E-3</td><td>1E-4</td><td>1E-5</td><td></td></tr>
   <tr>
     <td>simple_quadrature</td>
-    <td>94.0%</td><td>93.8%</td><td>93.6%</td><td>93.4%</td>
+    <td>96.4%</td><td>96.0%</td><td>95.8%</td><td>95.6%</td>
     <td></td>
   </tr>
   <tr>
@@ -179,7 +181,7 @@ implementations.
   <tr><td>Method</td><td>1E-1</td><td>1E-2</td><td>1E-3</td><td>1E-4</td><td></td></tr>
   <tr>
     <td>simple_quadrature</td>
-    <td>94.1%</td><td>93.7%</td><td>93.6%</td><td>93.4%</td>
+    <td>96.5%</td><td>95.9%</td><td>95.8%</td><td>95.6%</td>
     <td></td>
   </tr>
   <tr>
@@ -249,7 +251,7 @@ implementations.
   <tr><td>Method</td><td>1E-1</td><td>1E-2</td><td>1E-3</td><td>1E-4</td><td></td></tr>
   <tr>
     <td>simple_quadrature</td>
-    <td>99.9%</td><td>99.9%</td><td>99.9%</td><td>99.9%</td>
+    <td>100.0%</td><td>100.0%</td><td>100.0%</td><td>99.9%</td>
     <td></td>
   </tr>
   <tr>
@@ -290,7 +292,7 @@ TODO: truncated, not rounded. composite accuracy?
 
 ##### Where did these PDF/CDF tables come from? Are they accurate?
 
-TODO: Nolan quantile table may have some inaccuracies
+TODO: Nolan quantile table may have some inaccuracies, but this needs more testing to verify
 
 ##### What are some known limitations of this benchmark?
 
@@ -308,21 +310,27 @@ TODO: Mark Veillette says this well:
 >
 > One further annoyance is that the names of the 4 parameters are also inconsistent. [...] The letters alpha and beta are used almost everywhere you look, while the other two parameters are almost always different.
 
-TODO: will probably need to add tests to prove our transformations are good
+TODO: will need to finalize [tests to prove our transformations are good](tests/parameterization_tests.py)
 
 ##### pylevy_miotto appears somewhat accurate in practice. Why does it perform so poorly in this benchmark?
 
-TODO: pylevy_miotto parameter limitations
+TODO: pylevy_miotto parameter limitations and implementation bugs
 
 ##### simple_quadrature seems very accurate. Is this always the case?
 
-TODO: simple_quadrature limitations and potential hybrid scheme
+TODO: simple_quadrature limitations and potential hybrid scheme, should probably add the derivation for the integrands (or find our strategy in the literature somewhere)
 
 ##### simple_monte_carlo appears far slower in practice than listed here. Why?
 
 ##### These methods vary greatly in their speed. What is a "good" average time per call? 
 
-TODO: highly domain specific, etc.
+TODO: highly domain specific, include machine specs, etc.
+
+TODO: Nolan claims to have
+
+> code to quickly approximate stable densities.  This routine is much faster than the regular density calculations: approximately 1 million density evaluations/second can be performed on a 1 GHz Pentium.
+
+This appears to suggest that an average time per call of <1 us (!) is "easily" feasible on modern hardware. However, this is several orders of magnitude faster than any of the methods tested here.
 
 ##### Some of the methods only appear in the PDF or CDF tests. Why?
 
--- algorithms/simple_quadrature.py
+++ algorithms/simple_quadrature.py
@@ -2,47 +2,83 @@ from scipy.integrate import quad
 from numpy import exp, inf, pi, cos, sin, tan, log
 
 
-def pdf(x, alpha, beta, tol=1e-8, limit=10 ** 3):
+def _indefinite_trig_weighted_quad(f, epsabs, limit, weight, wvar):
+    """Integrates f(t)*weight(t*wvar) from 0 to infinity while handling some scipy issues/bugs."""
+    # scipy handles inf incorrectly, so we filter out t == 0. See https://github.com/scipy/scipy/issues/12662
+    f_int = lambda t: f(t) if t > 0 else 0
+
+    res, error = quad(f_int, 0, inf, epsabs=epsabs, limit=limit, weight=weight, wvar=wvar)
+
+    # need to check incorrect weighted integration results. See https://github.com/scipy/scipy/issues/12684
+    # TODO: this check does not always catch the issue. Can this be more robust?
+    #   the largest double is ~1e308 and I've observed res > 1e307 here. Everything above ~1.8e308 is
+    #   _supposed_ to represent NaN/inf, so maybe their code misinterprets this as a "real" value?
+    if res == 0.0 or error == 0.0 or res > 1e307 or abs(res) < error:
+        weight_f = cos if weight == "cos" else sin
+        res, error = quad(lambda t: f(t) * weight_f(wvar * t), 0, inf, epsabs=epsabs, limit=limit)
+
+    return res, error
+
+
+def pdf(x, alpha, beta, tol=1e-12, limit=10 ** 3):
     if beta == 0.0:
-        res, error = quad(lambda t: exp(-t ** alpha), 0, inf,
-                          epsabs=tol, limit=limit, weight="cos", wvar=x)
+        res, error = _indefinite_trig_weighted_quad(lambda t: exp(-t ** alpha),
+                                                    epsabs=tol, limit=limit, weight="cos", wvar=x)
         res = res / pi
     elif alpha == 1.0:
         phi_constant = -2 * beta / pi
-        res1, error1 = quad(lambda t: exp(-t) * cos(t * phi_constant * log(t)), 1e-10, inf,
-                            epsabs=tol, limit=limit, weight="cos", wvar=x)
-        res2, error2 = quad(lambda t: exp(-t) * sin(t * phi_constant * log(t)), 1e-10, inf,
-                            epsabs=tol, limit=limit, weight="sin", wvar=x)
+        res1, error1 = _indefinite_trig_weighted_quad(lambda t: exp(-t) * cos(t * phi_constant * log(t)),
+                                                      epsabs=tol, limit=limit, weight="cos", wvar=x)
+        res2, error2 = _indefinite_trig_weighted_quad(lambda t: exp(-t) * sin(t * phi_constant * log(t)),
+                                                      epsabs=tol, limit=limit, weight="sin", wvar=x)
         res = (res1 + res2) / pi
     else:
         phi_constant = beta * tan(pi * alpha / 2)
-        res1, error1 = quad(lambda t: exp(-t ** alpha) * cos(t ** alpha * phi_constant), 0, inf,
-                            epsabs=tol, limit=limit, weight="cos", wvar=x + phi_constant)
-        res2, error2 = quad(lambda t: exp(-t ** alpha) * sin(t ** alpha * phi_constant), 0, inf,
-                            epsabs=tol, limit=limit, weight="sin", wvar=x + phi_constant)
+
+        # TODO: I don't like this check, but it avoids catastrophic cancellation in these extreme cases
+        #   This is an analytic result, but you should probably reformulate to avoid this. In any case, this does not
+        #   significantly improve accuracy AND does not fix the issue when x is _very_ close to -phi_constant
+        if alpha < 1.0 and beta == 1.0 and x < -phi_constant:
+            return 0.0  # abrupt change at abs(beta) == 1.0 means the CDF is 0.0 here
+        if alpha < 1.0 and beta == -1.0 and x > -phi_constant:
+            return 0.0  # abrupt change at abs(beta) == 1.0 means the CDF is 1.0 here (so PDF is 0.0)
+
+        res1, error1 = _indefinite_trig_weighted_quad(lambda t: exp(-t ** alpha) * cos(t ** alpha * phi_constant),
+                                                      epsabs=tol, limit=limit, weight="cos", wvar=x + phi_constant)
+        res2, error2 = _indefinite_trig_weighted_quad(lambda t: exp(-t ** alpha) * sin(t ** alpha * phi_constant),
+                                                      epsabs=tol, limit=limit, weight="sin", wvar=x + phi_constant)
         res = (res1 + res2) / pi
 
-    # TODO: need to handle error == 0.0 bug in weighted quad(); will probably fallback to unweighted quad
     return max(res, 0)
 
 
-def cdf(x, alpha, beta, tol=1e-8, limit=10 ** 3):
+def cdf(x, alpha, beta, tol=1e-12, limit=10 ** 3):
     if beta == 0.0:
-        res, error = quad(lambda t: exp(-t ** alpha) / t if t != 0 else 0, 0, inf,
-                          epsabs=tol, limit=limit, weight="sin", wvar=x)
-        res = 1 / 2 + res / pi
+        res, error = _indefinite_trig_weighted_quad(lambda t: exp(-t ** alpha) / t,
+                                                    epsabs=tol, limit=limit, weight="sin", wvar=x)
+        res = (pi / 2 + res) / pi
     elif alpha == 1.0:
         phi_constant = -2 * beta / pi
-        res1, error1 = quad(lambda t: exp(-t) * cos(t * phi_constant * log(t)) / t if t != 0 else 0, 0, inf,
-                            epsabs=tol, limit=limit, weight="sin", wvar=x)
-        res2, error2 = quad(lambda t: exp(-t) * sin(t * phi_constant * log(t)) / t if t != 0 else 0, 0, inf,
-                            epsabs=tol, limit=limit, weight="cos", wvar=x)
-        res = 1 / 2 + (res1 - res2) / pi
+        res1, error1 = _indefinite_trig_weighted_quad(lambda t: exp(-t) * cos(t * phi_constant * log(t)) / t,
+                                                      epsabs=tol, limit=limit, weight="sin", wvar=x)
+        res2, error2 = _indefinite_trig_weighted_quad(lambda t: exp(-t) * sin(t * phi_constant * log(t)) / t,
+                                                      epsabs=tol, limit=limit, weight="cos", wvar=x)
+        res = (pi / 2 + res1 - res2) / pi
     else:
         phi_constant = beta * tan(pi * alpha / 2)
-        res1, error1 = quad(lambda t: exp(-t ** alpha) * cos(t ** alpha * phi_constant) / t if t != 0 else 0, 0, inf,
-                            epsabs=tol, limit=limit, weight="sin", wvar=x + phi_constant)
-        res2, error2 = quad(lambda t: exp(-t ** alpha) * sin(t ** alpha * phi_constant) / t if t != 0 else 0, 0, inf,
-                            epsabs=tol, limit=limit, weight="cos", wvar=x + phi_constant)
-        res = 1 / 2 + (res1 - res2) / pi
+
+        # TODO: I don't like this check, but it avoids catastrophic cancellation in these extreme cases
+        #   This is an analytic result, but you should probably reformulate to avoid this. In any case, this does not
+        #   significantly improve accuracy AND does not fix the issue when x is _very_ close to -phi_constant
+        if alpha < 1.0 and beta == 1.0 and x < -phi_constant:
+            return 0.0  # abrupt change at abs(beta) == 1.0 means the CDF is 0.0 here
+        if alpha < 1.0 and beta == -1.0 and x > -phi_constant:
+            return 1.0  # abrupt change at abs(beta) == 1.0 means the CDF is 1.0 here
+
+        res1, error1 = _indefinite_trig_weighted_quad(lambda t: exp(-t ** alpha) * cos(t ** alpha * phi_constant) / t,
+                                                      epsabs=tol, limit=limit, weight="sin", wvar=x + phi_constant)
+        res2, error2 = _indefinite_trig_weighted_quad(lambda t: exp(-t ** alpha) * sin(t ** alpha * phi_constant) / t,
+                                                      epsabs=tol, limit=limit, weight="cos", wvar=x + phi_constant)
+        res = (pi / 2 + res1 - res2) / pi
+
     return max(res, 0)
